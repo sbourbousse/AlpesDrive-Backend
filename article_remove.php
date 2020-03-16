10a1a3d220ebf5c9f4d64a6ef7d6440743a1f346
130065 <u98981947@cbi-bs1.server.lan>
2020-03-16 15:45:32 +0100

    Suppression d'un article

---
+++ article_remove.php
@@ -0,0 +1,43 @@
+<?php
+
+require 'Database.php'; // Classe Database
+require 'Article.php'; // Classe Article
+require 'Response.php';
+
+include 'function.php';
+
+header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *"); //TODO Modifier
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+
+//Création d'une réponse
+$response = new Response;
+//Connexion à la base de données
+try {
+    $dbh = new Database;
+    $response->setDb(true, "La connexion à la base de données a réussie");
+} catch (PDOException $e) {
+    $response->setDb(false, "La connexion à la base de données a échouée :  " . $e->getMessage());
+}
+//Récuperer les données
+$json = file_get_contents("php://input");
+$data = json_decode($json);
+
+//Liste des champs normalement envoyés
+$venteId = $data->{'idVente'};
+$clientId = $data->{'idClient'};
+
+
+if(!somethingMissing($venteId, $clientId)) {
+    $article = new Article($venteId, $clientId, $dbh);
+    if($article->removeFromDatabase($dbh)) {
+        $response->setDelete(true,"L'article à été supprimé'");
+
+    } else {
+        $response->setDelete(false,"La suppression à échoué");
+    }
+} else {
+    $response->setDelete(false,"Il manque un champs pour supprimer l'article du panier");
+}
+
+$response->printResponseJSON();
