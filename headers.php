26d172af9d35e7103e1fb56011407695c2c4c5c6
130065 <u98981947@cbi-bs1.server.lan>
2020-03-19 14:50:28 +0100

    la branche developpement accepte toute origines

--- headers.php
+++ headers.php
@@ -1,5 +1,5 @@
 <?php
 header('Content-Type: application/json');
-header("Access-Control-Allow-Origin: https://unruffled-allen-7ebff8.netlify.com"); //TODO Modifier
+header("Access-Control-Allow-Origin: *"); //TODO Modifier
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 header("Retry-After: 10");
